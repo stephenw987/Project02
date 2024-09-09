cbde58c24093407555c2705fd194fe5c8856ec73
Hammarou <139810496+Hammarou@users.noreply.github.com>
2024-09-09 15:12:35 -0400

    Finish implementing the use of environment variables for database connection

--- config/connection.js
+++ config/connection.js
@@ -10,11 +10,8 @@ if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL);
 } else {
   sequelize = new Sequelize(
-    "notetaker_db",
-    "postgres",
-    // "password",
-    // process.env.DB_NAME,
-    // process.env.DB_USER,
+    process.env.DB_NAME,
+    process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
       host: "localhost",
